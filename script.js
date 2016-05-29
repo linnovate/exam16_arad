a7f7188882271f49e40e9cc977bc4dea402e01e6
tel4 <tel4@tlalei.local>
2016-05-29 14:56:32 +0300

    adina manela 088001455

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var parent1
 var tree = [
 {
 	id : 1,
@@ -14,7 +15,7 @@ var tree = [
 {
 	id : 3,
 	title: "Aviva",
-	parentID: 1,
+	parentID: 2,
 	gender: "female"
 },
 {
@@ -173,3 +174,63 @@ var tree = [
 	parentID: 2,
 	gender: "male"
 }];
+ function a1()
+ {
+	  
+ var i=0 ;
+  var j=0 ;
+
+ //alert (tree[i].id);
+ var degel1=0;
+ var degel2=0;
+ for( i=0 ;i<tree.length && degel2==0;i++)
+ {
+	 degel1=0;
+ for(j=0;j<tree.length && degel1==0;j++)
+ {
+	 
+	 if (tree[i].parentID==tree[j].id)
+	 degel1=1;
+ }
+ if(degel1==0)
+	 degel2=1;
+ }
+ if(degel2==1)
+ {
+	  parent1=i-1;
+	 alert (tree[parent1].id);
+ }
+  alert(i-1);
+ }
+ 
+  function a2()
+ {
+	  var cparent=parent1;
+ alert(cparent);
+ var idp=tree[cparent].id;
+ var w=0;
+  var w1=0;
+ while(w<tree.length)
+ {
+  while(idp != tree[w1].parentID&&w1<tree.length)
+  {
+	  w1++;
+  }
+  if (idp == tree[w1].parentID)  
+  {
+	  alert (tree[w1].title);
+	  w++;
+	  //cparent=w1;
+	  idp=tree[w1].id;
+  }
+  else
+  {
+	  w1=0;
+	  idp=tree[w1].parentID;
+  }
+	  
+	  
+ }
+  }
+ 
+
